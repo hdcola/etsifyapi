10c12a3d2b0af2ce58cee15a8d92ac3b97637e68
hdcola <gh@hdcola.org>
2024-10-27 18:54:31 -0400

    init strip payment intent

--- app.js
+++ app.js
@@ -3,17 +3,20 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const cros = require('cors');
 
 const db = require('./models');
 db.sequelize.sync();
 
 const usersRouter = require('./routes/users');
+const paymentsRouter = require('./routes/payments');
 
 const errorHandler = require('./middlewares/error-handler');
 const ApiError = require('./utils/api-error');
 
 const app = express();
 
+app.use(cros());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -21,6 +24,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/users', usersRouter);
+app.use('/', paymentsRouter);
 
 app.use((req, res, next) => {
   const error = ApiError.notFound('Resource not found' + req.originalUrl);
