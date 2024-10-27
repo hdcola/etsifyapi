18aee156b82459d3cc16d93184a4877442c552bb
iasssy <iana.setrakova@johnabbottcollege.net>
2024-10-27 12:12:05 -0400

    Update error-handler.js - removed console.error(err)

--- middlewares/error-handler.js
+++ middlewares/error-handler.js
@@ -11,6 +11,5 @@ module.exports = (err, req, res, next) => {
     res.status(err.statusCode).json({ status: 'error', message: err.message });
     return;
   }
-  console.error(err);
   res.status(500).json({ status: 'error', message: 'Internal Server Error' });
 };
