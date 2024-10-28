aa0118aa4a45f75c69ee3ad42d9bf4e5b4e6d15e
iasssy <iana.setrakova@johnabbottcollege.net>
2024-10-28 18:45:26 -0400

    added tests for login

--- test/routers/user.test.js
+++ test/routers/user.test.js
@@ -5,6 +5,7 @@ const ApiError = require('../../utils/api-error');
 const { users } = require('../../models');
 const { Sequelize } = require('sequelize');
 const errorHandler = require('../../middlewares/error-handler');
+const bcrypt = require('bcrypt');
 
 const app = express();
 app.use(express.json());
@@ -89,7 +90,7 @@ describe('POST /api/users/register', () => {
         );
     });
 
-    it('should return 500 Internal Server Error when database connection fails', async () => {
+    it('it fail to register and return 500 Internal Server Error when database connection fails', async () => {
         const SequelizeConnectionError = new Sequelize.ConnectionError(
             'Database connection failed'
         );
@@ -113,16 +114,73 @@ describe('POST /api/users/register', () => {
         );
         expect(response.body).not.toHaveProperty('errors');
     });
+});
 
-    describe('POST /api/users/login', () => {
-        beforeAll(() => {
-            process.env.JWT_SECRET = 'testSecretKey';
-        });
-
-        afterEach(() => {
-            jest.clearAllMocks();
-        });
-
-        it('should sucessfully login', async () => {});
+describe('POST /api/users/login', () => {
+    beforeAll(() => {
+        process.env.JWT_SECRET = 'testSecretKey';
     });
+
+    afterEach(() => {
+        jest.clearAllMocks();
+    });
+    
+    it('it should sucessfully login', async () => {
+        const saltRounds = 10;
+        users.findOne.mockResolvedValue({
+            id: 1,
+            username: 'TestUser',
+            full_name: 'TestName TestFamilyName',
+            email: 'test@example.com',
+            password: await bcrypt.hash('securePassword', saltRounds),
+        });
+        const response = await request(app).post('/api/users/login').send({
+            email: 'test@example.com',
+            password: 'securePassword',
+        });
+
+        expect(response.status).toBe(200);
+        expect(response.body).toHaveProperty('success', true);
+        expect(response.body).toHaveProperty('message', 'Login successful');
+        expect(response.body).toHaveProperty('token', 'mockedToken');
+    });
+
+    it('it should fail to login with incorrect email or password', async () => {
+        users.findOne.mockResolvedValue(null);
+
+        const response = await request(app).post('/api/users/login').send({
+            email: 'test@example.com',
+            password: 'securePassword',
+        });
+
+        expect(response.status).toBe(401);
+        expect(response.body).toHaveProperty('status', 'error');
+        expect(response.body).toHaveProperty('message', 'Wrong email or password');
+    });
+
+    
+    it('it fail to log in and return 500 Internal Server Error when database connection fails', async () => {
+        const SequelizeConnectionError = new Sequelize.ConnectionError(
+            'Database connection failed'
+        );
+        SequelizeConnectionError.name = 'SequelizeConnectionError';
+
+        users.create.mockRejectedValue(SequelizeConnectionError);
+        const response = await request(app).post('/api/users/login').send({
+            email: 'test@example.com',
+            password: 'securePassword',
+        });
+
+        console.log('response.body:', response.body);
+
+        expect(response.status).toBe(500);
+        expect(response.body).toHaveProperty('status', 'error');
+        expect(response.body).toHaveProperty(
+            'message',
+            'Internal Server Error'
+        );
+        expect(response.body).not.toHaveProperty('errors');
+    });
+
+
 });
