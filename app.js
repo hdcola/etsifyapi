e265e16cd0d28ef39e7be923e8c92c88148acb6c
hdcola <gh@hdcola.org>
2024-10-29 10:11:01 -0400

    add first unit test

--- app.js
+++ app.js
@@ -1,37 +1,12 @@
 require('dotenv').config();
 const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
-const cros = require('cors');
 
 const db = require('./models');
 db.sequelize.sync();
 
-const usersRouter = require('./routes/users');
-const paymentsRouter = require('./routes/payments');
-
-const errorHandler = require('./middlewares/error-handler');
-const ApiError = require('./utils/api-error');
-
+const appSetup = require('./appSetup');
 const app = express();
-app.use(cors());
 
-app.use(cros());
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/users', usersRouter);
-app.use('/', paymentsRouter);
-
-app.use((req, res, next) => {
-    const error = ApiError.notFound('Resource not found' + req.originalUrl);
-    next(error);
-});
-
-app.use(errorHandler);
+appSetup(app);
 
 module.exports = app;
