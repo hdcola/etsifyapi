6049bec08fc1d29493ac4a0238549f4cf89f53df
Tamara Plante <0631064@johnabbottcollege.net>
2024-11-01 02:04:29 -0400

    Add JWT validation

---
+++ middlewares/jwt.js
@@ -0,0 +1,29 @@
+const { sign, verify } = require('jsonwebtoken');
+
+function generateToken(payload) {
+    const secretKey = process.env.JWT_SECRET || 'testSecretKey';
+
+    return sign(payload, secretKey, {
+        expiresIn: '10 days',
+    });
+}
+
+const validateToken = (req, res, next) => {
+    const secretKey = process.env.JWT_SECRET || 'testSecretKey';
+    const token = req.headers.authorization?.replace('Bearer ', '');
+
+    if (!token) {
+        return res.status(403).json({ error: 'Unauthorized' });
+    }
+
+    try {
+        const payload = verify(token, secretKey);
+        req.userId = payload.userId;
+        req.token = token;
+        next();
+    } catch (error) {
+        res.status(403).json({ error: 'Unauthorized ' });
+    }
+};
+
+module.exports = { generateToken, validateToken };
